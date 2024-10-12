ba8589b68a9ed091987fb0092fba6eea449e28d6
vok889 <vok889@gmail.com>
2024-10-12 11:46:13 +0700

    add item-entity title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ItemEntryComponent } from './budget/pages/item-entry/item-entry.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'budget/item-entry', component: ItemEntryComponent, title: 'Entry' }
+];
---
+++ src/app/budget/pages/item-entry/item-entry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-entry',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-entry.component.html',
+  styleUrl: './item-entry.component.scss'
+})
+export class ItemEntryComponent {
+
+}
