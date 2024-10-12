b1936f377350e300aa9f38217f99c822b4846d39
vok889 <vok889@gmail.com>
2024-10-12 15:14:34 +0700

    add connect api with httpClient

--- src/app/budget/pages/item-entry/item-entry.component.ts
+++ src/app/budget/pages/item-entry/item-entry.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Item, ItemStatus } from '../../models/item';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-item-entry',
@@ -11,45 +12,29 @@ import { map } from 'rxjs';
   styleUrl: './item-entry.component.scss'
 })
 export class ItemEntryComponent {
-  items: Item[] = [
-    {
-      title: "Gaming Laptop",
-      amount: 3,
-      price: 3200,
-      contactMobileNo: "0812345678",
-      status: ItemStatus.APPROVED,
-      id: 1
-    },
-    {
-      title: "Desktop Tower",
-      amount: 7,
-      price: 2500,
-      contactMobileNo: "0823456789",
-      status: ItemStatus.PENDING,
-      id: 2
-    },
-    {
-      title: "Mechanical Keyboard",
-      amount: 6,
-      price: 750,
-      contactMobileNo: "0834567890",
-      status: ItemStatus.REJECTED,
-      id: 3
-    }
-  ];
 
   isSmallTable = false;
+  httpClient = inject( HttpClient);
+
+  items: Item[] = [];
+
   filterItems = this.items;
   filterInput = new FormControl<string>('', { nonNullable: true })
 
   constructor() {
+
+    this.httpClient.get<Item[]>('http://localhost:3000/items').subscribe(vs => {
+      this.items = vs;
+      this.filterItems = vs;
+    })
+
     this.filterInput.valueChanges
       .pipe(map(keyword => keyword.toLocaleLowerCase()))
       .subscribe(keyword => {
         const numericKeyword = parseInt(keyword, 10);
 
         this.filterItems = this.items.filter(item => item.title.toLocaleLowerCase().includes(keyword)
-        // || item.status.toLocaleLowerCase().includes(keyword)
+        || item.status.toLocaleLowerCase().includes(keyword)
         || item.amount === numericKeyword
       )
       })
