20db86b547a661857d90514f96245948c0314286
vok889 <vok889@gmail.com>
2024-10-19 16:40:38 +0700

    add roles guard

---
+++ src/app/auth/guards/roles.guard.ts
@@ -0,0 +1,20 @@
+// roles.guard.ts
+import { CanActivateFn, Router } from '@angular/router';
+import { Role } from '../models/logged-in-user';
+import { AuthService } from '../auth.service';
+import { inject } from '@angular/core';
+
+export function rolesGuard(roles: Role[]): CanActivateFn {
+  return (route, state) => {
+  
+    const loggedInUser = inject(AuthService).loggedInUser
+
+    if (loggedInUser && roles.includes(loggedInUser?.userProfile.role)) {
+      return true;
+    }
+    alert('You do not have permission to access this page');
+  
+    inject(Router).navigate(['/']);
+    return false;
+  }
+};
--- src/app/budget/budget.routes.ts
+++ src/app/budget/budget.routes.ts
@@ -3,12 +3,19 @@ import { Routes } from '@angular/router';
 import { ItemEntryComponent } from './pages/item-entry/item-entry.component';
 import { ItemFormComponent } from './pages/item-form/item-form.component';
 import { ItemApprovalComponent } from './pages/item-approval/item-approval.component';
+import { rolesGuard } from '../auth/guards/roles.guard';
+import { Role } from '../auth/models/logged-in-user';
 
 export const routes: Routes = [
   { path: 'item-entry', component: ItemEntryComponent, title: 'Entry' },
   { path: 'item-add', component: ItemFormComponent, title: 'Add' },
   { path: 'item-edit/:id', component: ItemFormComponent, title: 'Edit' },
-  { path: 'item-approval', component: ItemApprovalComponent, title: 'Approval' },
+  { 
+    path: 'item-approval', 
+    component: ItemApprovalComponent, 
+    title: 'Approval',
+    canActivate: [rolesGuard([Role.ADMIN, Role.MANAGER])]
+   },
 ];
 
 export default routes;
